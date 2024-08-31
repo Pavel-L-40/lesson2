0b55975f25a925390a19827e023af1516dfcecbe
PAvel <95528324+Pavel-L-40@users.noreply.github.com>
2024-08-31 22:07:13 +0300

    start

---
+++ lesson2.py
@@ -0,0 +1,6 @@
+# Количество выполненных ДЗ (запишите значение 12)
+count_task = 12
+time_tasks = 1.5
+develop = 'Python'
+print(f'Курс: {develop}, всего задач: {count_task}, затрачено часов: {time_tasks}, среднее время выполнения {time_tasks/count_task} часа.')
+print('Курс: ' + develop + ', всего задач: ' + str(count_task) + ', затрачено часов: ' + str(time_tasks) + ', среднее время выполнения ' + str(time_tasks/count_task) + ' часа.')
